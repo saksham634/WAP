47295f517e43b04a499071a533d813a5d238a432
Coder-Spark <prafful.1923it1166@kiet.edu>
2026-06-30 02:17:51 +0530

    Implemented Active navigation State Management

--- shared/js/layout-loader.js
+++ shared/js/layout-loader.js
@@ -1,6 +1,36 @@
-fetch("/shared/components/sidebar.html")
+console.log("LAYOUT LOADER STARTED");
+
+fetch("../../../shared/components/sidebar.html")
     .then(res => res.text())
     .then(html => {
+
         document.getElementById("global-sidebar").innerHTML = html;
-    })
-    .catch(err => console.error("Sidebar load failed:", err));
+
+        console.log("Sidebar Loaded");
+
+        const currentPage = window.location.pathname
+            .split("/")
+            .pop()
+            .replace(".html", "");
+
+        console.log("Current Page =", currentPage);
+
+        document.querySelectorAll(".sidebar a[data-page]").forEach(link => {
+
+            console.log(
+                "Link =", link.dataset.page,
+                "| Equal? =", link.dataset.page === currentPage
+            );
+
+            if (link.dataset.page === currentPage) {
+
+                console.log("MATCH FOUND!");
+
+                link.parentElement.classList.add("active");
+            }
+
+        });
+
+        console.log(document.querySelector(".sidebar-menu li.active"));
+
+    });

cc3fc77a3584cfd840a73dc9534a28e51c83b472
Coder-Spark <prafful.1923it1166@kiet.edu>
2026-06-30 02:26:18 +0530

    removed trash files and folders

--- employee-service/employee-service/src/main/java/com/wap/employee_service/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wap.employee_service;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-    // Inherits all fundamental CRUD operations automatically
-}

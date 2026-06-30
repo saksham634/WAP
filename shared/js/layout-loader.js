6f4d8a43910dec2d63189dc0349520d1762fdcfa
Coder-Spark <prafful.1923it1166@kiet.edu>
2026-06-30 12:16:36 +0530

    Used Global functions and code for hr Module

--- shared/js/layout-loader.js
+++ shared/js/layout-loader.js
@@ -1,36 +1,25 @@
-console.log("LAYOUT LOADER STARTED");
-
-fetch("../../../shared/components/sidebar.html")
+const sidebarContainer = document.getElementById("global-sidebar");
+const sidebarType = sidebarContainer.dataset.sidebar;
+fetch(`../../../shared/components/sidebar/${sidebarType}.html`)
     .then(res => res.text())
     .then(html => {
-
         document.getElementById("global-sidebar").innerHTML = html;
-
         console.log("Sidebar Loaded");
-
         const currentPage = window.location.pathname
             .split("/")
             .pop()
             .replace(".html", "");
-
         console.log("Current Page =", currentPage);
-
         document.querySelectorAll(".sidebar a[data-page]").forEach(link => {
-
             console.log(
                 "Link =", link.dataset.page,
                 "| Equal? =", link.dataset.page === currentPage
             );
 
             if (link.dataset.page === currentPage) {
-
                 console.log("MATCH FOUND!");
-
                 link.parentElement.classList.add("active");
             }
-
         });
-
         console.log(document.querySelector(".sidebar-menu li.active"));
-
     });

8b90a83ce6de78efcfc75b52c34ce5d15898fa37
Coder-Spark <prafful.1923it1166@kiet.edu>
2026-06-30 12:53:39 +0530

    Created Admin Module and continuning it

--- shared/js/layout-loader.js
+++ shared/js/layout-loader.js
@@ -1,16 +1,25 @@
 const sidebarContainer = document.getElementById("global-sidebar");
 const sidebarType = sidebarContainer.dataset.sidebar;
+
 fetch(`../../../shared/components/sidebar/${sidebarType}.html`)
     .then(res => res.text())
     .then(html => {
-        document.getElementById("global-sidebar").innerHTML = html;
+        sidebarContainer.innerHTML = html;
         console.log("Sidebar Loaded");
         const currentPage = window.location.pathname
             .split("/")
             .pop()
             .replace(".html", "");
         console.log("Current Page =", currentPage);
+
+        // Remove existing active classes
+        document.querySelectorAll(".sidebar-menu li").forEach(item => {
+            item.classList.remove("active");
+        });
+
+        // Add active class to matching page
         document.querySelectorAll(".sidebar a[data-page]").forEach(link => {
+
             console.log(
                 "Link =", link.dataset.page,
                 "| Equal? =", link.dataset.page === currentPage
@@ -22,4 +31,7 @@ fetch(`../../../shared/components/sidebar/${sidebarType}.html`)
             }
         });
         console.log(document.querySelector(".sidebar-menu li.active"));
+    })
+    .catch(error => {
+        console.error("Sidebar failed to load:", error);
     });
